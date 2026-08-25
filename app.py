fc66ff123d7c851ca97e656a205ea1691e57444f
KarthikNasa <karthiknasa15@gmail.com>
2026-08-25 22:31:55 +0530

    Use environment variable for port configuration

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+import os 
 
 app = Flask(__name__)
 
@@ -139,10 +140,13 @@ def weather():
         }), 500
 
 
+
 if __name__ == "__main__":
+
+    port = int(os.environ.get("PORT", 5000))
 
     app.run(
         host="0.0.0.0",
-        port=5000,
+        port=port,
         debug=True
     )
